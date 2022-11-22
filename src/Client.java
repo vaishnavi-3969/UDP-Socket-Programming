38d07e777f239f566ba7b2b88b5bdbbcf2224377
Vaishnavi Arun Kale <vaishnavi.kale3011@gmail.com>
2022-11-23 00:19:38 +0530

    Updated

--- src/Client.java
+++ src/Client.java
@@ -15,19 +15,12 @@ class Client {
         String sentence = null;
         while (!Objects.equals(sentence, "bye")) {
             sentence = inFromUser.readLine();
-
             sendData = sentence.getBytes();
-
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 7009);
-
             clientSocket.send(sendPacket);
-
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-
             clientSocket.receive(receivePacket);
-
             String modifiedSentence = new String(receivePacket.getData());
-
             System.out.println("FROM SERVER:" + modifiedSentence);
         }
         System.out.println(":)");
--- src/Server.java
+++ src/Server.java
@@ -5,24 +5,15 @@ class Server {
         DatagramSocket serverSocket = new DatagramSocket(7009);
         byte[] receiveData = new byte[1024];
         byte[] sendData = new byte[1024];
-
         while (true) {
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-
             serverSocket.receive(receivePacket);
-
             String sentence = new String(receivePacket.getData());
-
             InetAddress IPAddress = receivePacket.getAddress();
-
             int port = receivePacket.getPort();
-
             String capitalizedSentence = sentence.toUpperCase();
-
             sendData = capitalizedSentence.getBytes();
-
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-
             System.out.println("Sending Capitalized message to Client");
             serverSocket.send(sendPacket);
         }
