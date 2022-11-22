e498a692265c86d2dd11a024e4a2ccb166069468
Vaishnavi Arun Kale <vaishnavi.kale3011@gmail.com>
2022-11-23 00:18:56 +0530

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.*;
+import java.util.Objects;
+
+class Client {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Enter your message: (To exit type 'bye'");
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName("localhost");
+
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+        String sentence = null;
+        while (!Objects.equals(sentence, "bye")) {
+            sentence = inFromUser.readLine();
+
+            sendData = sentence.getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 7009);
+
+            clientSocket.send(sendPacket);
+
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+            clientSocket.receive(receivePacket);
+
+            String modifiedSentence = new String(receivePacket.getData());
+
+            System.out.println("FROM SERVER:" + modifiedSentence);
+        }
+        System.out.println(":)");
+        clientSocket.close();
+    }
+}
